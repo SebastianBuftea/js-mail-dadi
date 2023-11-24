bafbcd831e443a0f6bb5fd7669f9551270705f72
SebastianBuftea <sebibuftea85@gmail.com>
2023-11-24 09:48:52 +0100

    [UPDATE]

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,13 +5,17 @@ let mail= prompt("inserisci la tua mail");
 let list_mail= ["ciccio@email.com","pippo@email.com","aldo@email.com","giacomo@email.com","giovanni@email.com","francesco@email.com","luigi@email.com"];
 let size= list_mail.length;
 
+let flag= false;
 //creiamo un ciclo for che controlli se l indirizzio mail inserito è presente  nel array.
 
 for(i=0; i<size; i++){
     if(list_mail[i] == mail){
-        alert("La tua mail risulta nella nostra base dati");
+       flag= true;
     }
-    else{
+   
+    if(flag == true){
+          alert("La tua mail risulta nella nostra base dati");
+    } else{
         alert("Ci dispiace, la tua mail non risulta nella nostra base dati");
     }
 
